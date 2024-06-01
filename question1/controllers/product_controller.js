0e744324c254634c37775c70a64c9a2e06c3a94e
Aditya Tyagi <adityatyagiav1@gmail.com>
2024-06-01 11:20:21 +0530

    route controller defined

---
+++ question1/controllers/product_controller.js
@@ -0,0 +1,25 @@
+
+export async function GetProducts(req, res) {
+    const { categoryname } = req.params;
+    const { top, minPrice, maxPrice, page = 1, sortBy, sortOrder } = req.query;
+
+    try {
+        const products = await getProducts(categoryname, top, minPrice, maxPrice, page, sortBy, sortOrder);
+        res.json(products);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+export async function GetProductById (req, res) {
+    const { categoryname, productid } = req.params;
+
+    try {
+        const product = await getProductById(categoryname, productid);
+        res.json(product);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+module.exports = router;
